a3defa21f7aef4c73b273e1e11d159a5cb419548
NvDzung <dung3032003md@gmail.com>
2024-08-07 10:24:49 +0700

    First commit

---
+++ src/test/java/com/practice/from_scratch/FromScratchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.from_scratch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FromScratchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
